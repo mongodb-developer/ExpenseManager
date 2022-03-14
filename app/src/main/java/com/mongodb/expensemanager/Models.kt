85c7b4391dbd55b2a3e7ae49d599943838494dcc
Mohit Sharma <mohit7575@gmail.com>
2022-03-14 23:20:52 +0530

    primary key added to model

--- app/src/main/java/com/mongodb/expensemanager/Models.kt
+++ app/src/main/java/com/mongodb/expensemanager/Models.kt
@@ -1,10 +1,12 @@
 package com.mongodb.expensemanager
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
 open class ExpenseInfo : RealmObject {
 
+    @PrimaryKey
     var expenseId: String = UUID.randomUUID().toString()
     var expenseName: String = ""
     var expenseValue: Int = 0
